800e89d97dbafc15c29f7bac8c545b250ffa4313
rmelchorv <rmelchorv@gmail.com>
2015-09-11 14:22:02 -0500

    5th revision

--- FilosofosComensales/Main.Designer.cs
+++ FilosofosComensales/Main.Designer.cs
@@ -28,6 +28,8 @@
 		private void InitializeComponent()
 		{
 			this.lbConsola = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// lbConsola
@@ -38,16 +40,26 @@
 			this.lbConsola.Size = new System.Drawing.Size(466, 160);
 			this.lbConsola.TabIndex = 0;
 			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(183, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(94, 85);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(471, 401);
+			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.lbConsola);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Name = "Main";
 			this.Text = "Main";
 			this.Load += new System.EventHandler(this.Main_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -55,5 +67,6 @@
 		#endregion
 
 		private System.Windows.Forms.ListBox lbConsola;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
